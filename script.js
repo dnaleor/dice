cff879264b7ef97bd13a8fd8eea16090980c766e
Brendan Telzrow <telzrowb@gmail.com>
2015-07-03 09:24:06 -0500

    Initial commit.

---
+++ script.js
@@ -0,0 +1,30 @@
+var yourRoll = prompt("Please enter a number between 1 and 6.\nIf you correctly guess my roll, you win!\nIf you want to quit the game, just hit 'Cancel'.");
+	
+if(yourRoll >= 1 && yourRoll <= 6 && yourRoll % 1 == 0) {
+		var myRoll = Math.floor(Math.random()*6 + 1);
+		if(yourRoll == myRoll){
+			alert("Congratulations, you win! \n" + "I rolled a " + myRoll + " and you rolled a " + yourRoll + ".");
+		}
+	while(yourRoll != myRoll) {
+		alert("Sorry, you lose. \n" + "I rolled a " + myRoll + " and you rolled a " + yourRoll + ".");
+		var yourRoll = prompt("Let's try again! Please enter a number between 1 and 6.\n" + "Or, click 'Cancel' to end the game.");
+		var myRoll = Math.floor(Math.random()*6 + 1);
+		if(yourRoll == myRoll) {
+			alert("Congratulations, you win! \n" + "I rolled a " + myRoll + " and you rolled a " + yourRoll + ".");
+			yourRoll = myRoll;
+		}
+		else if (yourRoll == undefined) {
+			alert("Thanks for playing!");
+			yourRoll = myRoll;
+		}
+	}
+} 
+
+else if (yourRoll == undefined) {
+	alert("Thanks for playing!");
+}
+
+else {
+	alert("That is not a valid entry. Please refresh to try again.");
+}
+
