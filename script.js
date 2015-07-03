21552e74a9c34c9e3d0b9206d1b3aaa24f5b2bfc
Brendan Telzrow <telzrowb@gmail.com>
2015-07-03 15:12:01 -0500

    Add function for rolLDice

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 var yourRoll = prompt("Please enter a number between 1 and 6.\nIf you correctly guess my roll, you win!\nIf you want to quit the game, just hit 'Cancel'.");
+var rollDice = function () {
+	return Math.floor(Math.random() * 6 + 1);
+}
 	
 if(yourRoll >= 1 && yourRoll <= 6 && yourRoll % 1 == 0) {
-		var myRoll = Math.floor(Math.random()*6 + 1);
+		var myRoll = rollDice();
 		if(yourRoll == myRoll){
 			alert("Congratulations, you win! \n" + "I rolled a " + myRoll + " and you rolled a " + yourRoll + ".");
 		}
 	while(yourRoll != myRoll) {
 		alert("Sorry, you lose. \n" + "I rolled a " + myRoll + " and you rolled a " + yourRoll + ".");
 		var yourRoll = prompt("Let's try again! Please enter a number between 1 and 6.\n" + "Or, click 'Cancel' to end the game.");
-		var myRoll = Math.floor(Math.random()*6 + 1);
+		var myRoll = rollDice();
 		if(yourRoll == myRoll) {
 			alert("Congratulations, you win! \n" + "I rolled a " + myRoll + " and you rolled a " + yourRoll + ".");
 			yourRoll = myRoll;
