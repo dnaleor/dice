09f6fe0aaf3808a3caab8cf55f32b2f0ed564ba6
Brendan Telzrow <telzrowb@gmail.com>
2015-07-06 20:59:06 -0500

    Add games played and win/loss counters.

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ var rollDice = function () {
 	return Math.floor(Math.random() * 6 + 1);
 }
 
+// Win function
+var win = function () {
+	numberOfWins += 1;
+	document.getElementById("wins").innerHTML = ("Wins: " + numberOfWins);
+}
+
+var wins = 0;
+var losses = 0;
+
 // user selection of 1-6 or random number
 function setNumber (x) {
 	document.getElementById("yourRoll").innerHTML = ("Your Roll: " + x + ".");
@@ -10,12 +19,21 @@ function setNumber (x) {
 	document.getElementById("myRoll").innerHTML = ("My Roll: " + myNum + ".");
 	if (x == myNum) {
 		document.getElementById("result").innerHTML = ("Result: You win!");
+		wins += 1;
 	} else {
 		document.getElementById("result").innerHTML = ("Result: Sorry, please try again.");
+		losses += 1;
 	}
+	document.getElementById("winloss").innerHTML = ("Wins / Losses: " + wins + " / " + losses);
 };
 
+// Counts number of games played
+var gamesPlayed = 0;
 
+var counter = function () {
+	gamesPlayed += 1;
+	document.getElementById("counter").innerHTML = ("Games Played: " + gamesPlayed);
+};
 
 
 
